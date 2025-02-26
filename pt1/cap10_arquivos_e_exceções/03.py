963773d634d31f93876fd1ef76b4c89e0755d1ac
Maria Helena Varela <helena.varelakg@gmail.com>
2025-02-26 15:38:57 -0300

    arquivos e exceções

--- pt1/cap10_arquivos_e_exceções/03.py
+++ pt1/cap10_arquivos_e_exceções/03.py
@@ -1,8 +1,7 @@
 # 10.3 – Convidado: Escreva um programa que pergunte o nome ao usuário.
 # Quando ele responder, escreva o nome em um arquivo chamado guest.txt.
 
-file_path = '/home/helena/Desktop/curso-python/pt1/cap10_arquivos_e_exceções/guest.txt'
 nome = input('Qual o seu nome? ')
 
-with open(file_path, 'w') as file_object:
+with open('guest.txt', 'w') as file_object:
     file_object.write(nome.title())
