60f9d96c8821d0dce17e795a25f09db69ca735c8
Maria Helena Varela <helena.varelakg@gmail.com>
2025-02-26 15:09:29 -0300

    analisando textos

--- pt1/cap10_arquivos_e_exceções/10.py
+++ pt1/cap10_arquivos_e_exceções/10.py
@@ -1,15 +1,25 @@
 # 10.10 – Palavras comuns: Acesse o Projeto Gutenberg (http://gutenberg.org/ ) e
-# encontre alguns textos que você gostaria de analisar. Faça download dos arquivostexto dessas obras ou copie o texto puro de seu navegador para um arquivo-texto
-# em seu computador.
+# encontre alguns textos que você gostaria de analisar. Faça download dos arquivos texto dessas obras 
+# ou copie o texto puro de seu navegador para um arquivo-texto em seu computador.
 # Você pode usar o método count() para descobrir quantas vezes uma palavra ou
 # expressão aparece em uma string. Por exemplo, o código a seguir conta quantas
 # vezes a palavra 'row' aparece em uma string:
+# >>> line = "Row, row, row your boat"
+# >>> line.count('row')
+# 2
+# >>> line.lower().count('row')
+# 3
+# Observe que converter a string para letras minúsculas usando lower() faz com
+# que todas as formas da palavra que você está procurando sejam capturadas,
+# independentemente do modo como elas estiverem grafadas.
+# Escreva um programa que leia os arquivos que você encontrou no Projeto
+# Gutenberg e determine quantas vezes a palavra 'the' aparece em cada texto.
 
-alice = r'C:\Users\lelena\Documents\curso-python\pt1\cap10_arquivos_e_exceções\alice.txt'
-
-with open(alice) as object:
+with open('alice.txt') as object:
     conteudo = object.read()
 
-word = object.split()
-num = len(word)
-print(num)
+print("Número de caracteres no capítulo de Alice:")
+print(len(conteudo))
+
+print('Número de vezes que o nome "the" aparece:')
+print(conteudo.lower().count('the'))
